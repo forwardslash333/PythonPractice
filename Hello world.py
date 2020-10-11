6bcc3cf2ad349b40ef0d6e77bad66fe0d57da0b0
forwardslash333 <jacob.abhishek7@yahoo.com>
2020-10-11 12:57:56 +0530

    Hello world Python program

---
+++ Hello world.py
@@ -0,0 +1,8 @@
+print('Hello World') #simple print command usage.
+
+a = int(input ('Enter any number to add \n'))
+b = int(input ('Enter second number to add \n'))
+
+a= a+b
+
+print('The Sum of these two numbers is\n', a, sep='') #(sep)This removes space between string of print command and variable value.
