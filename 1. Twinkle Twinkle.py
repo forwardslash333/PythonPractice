28c171cd2d2f8406935260d0524f100da7ac7e04
forwardslash333 <jacob.abhishek7@yahoo.com>
2020-10-11 13:32:45 +0530

    Second program

--- 1. Twinkle Twinkle.py
+++ 1. Twinkle Twinkle.py
@@ -4,6 +4,6 @@
 		        Up above the world so high,   		
 		        Like a diamond in the sky. 
         Twinkle, twinkle, little star, 
-	        How I wonder what you are       '''
+	        How I wonder what you are       ''' #	@Jacob
 
 print("Twinkle, twinkle, little star,\n\tHow I wonder what you are!\n\t\tUp above the world so high,\n\t\tLike a diamond in the sky.\nTwinkle, twinkle, little star,\n\tHow I wonder what you are!")
---
+++ 2. Check Python Version.py
@@ -0,0 +1,6 @@
+#   Write a Python program to get the Python version you are using. @Jacob
+
+import sys
+print('The Python you are using is \n')
+print(sys.version)
+print(sys.version_info)
