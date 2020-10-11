11b47dfdd1db1b77f4e53f4bd1ed4acbfc9d4303
forwardslash333 <jacob.abhishek7@yahoo.com>
2020-10-11 14:17:11 +0530

    Practice

---
+++ 3. Time and Date.py
@@ -0,0 +1,10 @@
+''' Write a Python program to display the current date and time.
+Sample Output :
+Current date and time :
+2014-07-05 14:34:14 '''     # @Jacob
+
+import datetime
+
+print('Current Date and Time is: \n')
+
+print(str(datetime.datetime.now()))
---
+++ 4. Radius of circle.py
@@ -0,0 +1,8 @@
+#   Basic Input output program
+'''Write a Python program which accepts the radius of a circle from the user and compute the area.'''
+
+radius = float(input('Enter the radius of circle to calculate its area.\n'))
+
+area = 3.14159 * radius**2
+
+print(area)
